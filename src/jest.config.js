39009ad292764e35aff4310369282f1a7d9e8d26
jeffors212 <42345368+jeffors212@users.noreply.github.com>
2024-01-10 18:39:35 -0300

    add teste
    instalei o jest para testes

---
+++ src/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'vue'],
+    transform: {
+      '^.+\\.vue$': 'vue-jest',
+      '^.+\\.js$': 'babel-jest'
+    },
+    testMatch: ['**/tests/unit/**/*.spec.js']
+    // outras configurações necessárias
+  };
+  
